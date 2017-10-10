72dc0a57184a9e46e87b2ed70dcc3006e060d697
Peter van 't Hof <p.j.van_t_hof@lumc.nl>
2017-10-10 21:22:25 +0200

    Added template to all tools

---
+++ src/test/scala/nl/biopet/tools/mpileuptovcf/MpileupToVcfTest.scala
@@ -0,0 +1,13 @@
+package nl.biopet.tools.mpileuptovcf
+
+import nl.biopet.test.BiopetTest
+import org.testng.annotations.Test
+
+object MpileupToVcfTest extends BiopetTest {
+  @Test
+  def testNoArgs(): Unit = {
+    intercept[IllegalArgumentException] {
+      ToolTemplate.main(Array())
+    }
+  }
+}
