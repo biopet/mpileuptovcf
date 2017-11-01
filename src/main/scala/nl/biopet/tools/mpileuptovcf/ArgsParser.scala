dccb9febaa317912b30e23bc5ba0c40d5efd0bd9
rhpvorderman <r.h.p.vorderman@lumc.nl>
2017-11-01 16:46:55 +0100

    start on argparser desriptions

--- src/main/scala/nl/biopet/tools/mpileuptovcf/ArgsParser.scala
+++ src/main/scala/nl/biopet/tools/mpileuptovcf/ArgsParser.scala
@@ -13,23 +13,23 @@ class ArgsParser(cmdName: String) extends AbstractOptParser[Args](cmdName) {
   } text "out is a required file property"
   opt[String]('s', "sample") required () action { (x, c) =>
     c.copy(sample = x)
-  }
+  } text ""
   opt[Int]("minDP") action { (x, c) =>
     c.copy(minDP = x)
-  }
+  } text ""
   opt[Int]("minAP") action { (x, c) =>
     c.copy(minAP = x)
-  }
+  } text ""
   opt[Double]("homoFraction") action { (x, c) =>
     c.copy(homoFraction = x)
-  }
+  } text ""
   opt[Int]("ploidy") action { (x, c) =>
     c.copy(ploidy = x)
-  }
+  } text "Specify the ploidy as a number: '1' for haploid, '2' for diploid etc."
   opt[Double]("seqError") action { (x, c) =>
     c.copy(seqError = x)
-  }
+  } text ""
   opt[Unit]("refCalls") action { (_, c) =>
     c.copy(refCalls = true)
-  }
+  } text ""
 }
