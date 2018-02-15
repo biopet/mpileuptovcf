a7ebff005f917eade8b010bb5a50e5e068e50473
pjvan_thof <pjrvanthof@gmail.com>
2018-02-15 15:50:13 +0100

    auto format

--- src/test/scala/nl/biopet/tools/mpileuptovcf/MpileupToVcfTest.scala
+++ src/test/scala/nl/biopet/tools/mpileuptovcf/MpileupToVcfTest.scala
@@ -1,3 +1,24 @@
+/*
+ * Copyright (c) 2014 Biopet
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+ * the Software, and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+ * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+ * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
 package nl.biopet.tools.mpileuptovcf
 
 import java.io.File
@@ -25,7 +46,8 @@ class MpileupToVcfTest extends ToolTest[Args] {
   def testMain(): Unit = {
     val tmp = File.createTempFile("mpileup", ".vcf")
     tmp.deleteOnExit()
-    val args = Array("-I", pileup, "--sample", "test", "-o", tmp.getAbsolutePath)
+    val args =
+      Array("-I", pileup, "--sample", "test", "-o", tmp.getAbsolutePath)
 
     MpileupToVcf.main(args)
   }
@@ -35,15 +57,15 @@ class MpileupToVcfTest extends ToolTest[Args] {
     val tmp = File.createTempFile("mpileup", ".vcf")
     tmp.deleteOnExit()
     val args = Array("-I",
-      pileup,
-      "--sample",
-      "test",
-      "-o",
-      tmp.getAbsolutePath,
-      "--minDP",
-      "1",
-      "--minAP",
-      "1")
+                     pileup,
+                     "--sample",
+                     "test",
+                     "-o",
+                     tmp.getAbsolutePath,
+                     "--minDP",
+                     "1",
+                     "--minAP",
+                     "1")
     MpileupToVcf.main(args)
 
     val vcfReader = new VCFFileReader(tmp, false)
@@ -60,15 +82,15 @@ class MpileupToVcfTest extends ToolTest[Args] {
     val tmp = File.createTempFile("mpileup", ".vcf")
     tmp.deleteOnExit()
     val args = Array("-I",
-      pileup,
-      "--sample",
-      "test",
-      "-o",
-      tmp.getAbsolutePath,
-      "--minDP",
-      "1",
-      "--minAP",
-      "1")
+                     pileup,
+                     "--sample",
+                     "test",
+                     "-o",
+                     tmp.getAbsolutePath,
+                     "--minDP",
+                     "1",
+                     "--minAP",
+                     "1")
     MpileupToVcf.main(args)
 
     val vcfReader = new VCFFileReader(tmp, false)
@@ -84,7 +106,9 @@ class MpileupToVcfTest extends ToolTest[Args] {
       refAlleles.size should be >= 1
 
       val realRef = Allele.create(
-        sequenceFile.getSubsequenceAt(record.getContig, record.getStart, record.getEnd).getBases,
+        sequenceFile
+          .getSubsequenceAt(record.getContig, record.getStart, record.getEnd)
+          .getBases,
         true)
 
       for (ref <- refAlleles) {
